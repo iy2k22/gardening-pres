4b5bdd99ee56845c54816588d56e5fe092096cfd
Ibrahim Yusuf <iy830755@gmail.com>
2023-12-21 20:04:59 +0000

    add forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,9 +26,29 @@ $(document).ready(() => {
                 })
                 .then(({ list }) => {
                     const relevant = list.filter(({ dt_txt }) => dt_txt.slice(11, 13) === '12');
+                    console.log(relevant);
                     for (let forecast of relevant) {
                         const forecastCard = $("<div></div>");
-                        forecastCard.addClass("card col-lg-2");
+                        forecastCard.addClass("card col-lg-2 col-md-2 col-sm-12");
+                        const cardBody = $("<div></div>");
+                        cardBody.addClass("card-body");
+                        const day = $("<h5></h5>");
+                        day.addClass("card-title");
+                        day.text(dayjs(forecast.dt_txt).format("dddd, MMMM Do"));
+                        const icon = $("<img>");
+                        icon.attr("src", `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`);
+                        icon.addClass("card-img-top");
+                        icon.attr("alt", forecast.weather[0].description);
+                        const temp = $("<p></p>");
+                        temp.text((forecast.main.temp - 273.15).toFixed(2));
+                        temp.addClass("temperature card-text");
+                        
+                        cardBody.append(day);
+                        cardBody.append(icon);
+                        cardBody.append(temp);
+                        
+                        forecastCard.append(cardBody);
+                        forecastDiv.append(forecastCard);
                     }
                 })
         }, (e) => $("#forecastBit").remove());
